ba3a0de31932dfafb67ad9c39c1f9d8c97ff0879
Sam-Mauris Yong <mauris@hotmail.sg>
2013-07-21 16:08:58 +0800

    removed composer.pp and added apache provisioning

--- modules/extras/manifests/init.pp
+++ modules/extras/manifests/init.pp
@@ -1,6 +1,10 @@
 class extras{
     class { ['apache2::mod::php', 'apache2::mod::rewrite']: }
 
+    class { "apache":
+      version =>  '2.2.22'
+    }
+
     php::module { ['curl', 'xdebug', 'gd']:
         notify => Service['apache2']
     }
