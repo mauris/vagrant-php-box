b6c5677fcc5c25141a62fb82778c7fc1aa83f10f
Sam-Mauris Yong <mauris@hotmail.sg>
2013-07-21 12:21:30 +0800

    updated composer loading

--- modules/php/manifests/composer.pp
+++ modules/php/manifests/composer.pp
@@ -1,25 +1,18 @@
 class php::composer {
   include php
 
-# download composer
   exec { "download composer":
     path => "/usr/bin:/bin:/usr/local/bin",
-    command => "curl -sS https://getcomposer.org/installer | php -- --install-dir=/home/vagrant",
-    require => Package["curl", "php5-cli"]
+    command => "wget http://getcomposer.org/composer.phar -O /usr/local/bin/composer",
+    require => Package["curl", "php5-cli"],
+    creates => '/usr/local/bin/composer'
   }
 
 # install composer
-  file { "/usr/local/bin/composer":
-    ensure => present,
-    source => "/home/vagrant/composer.phar",
-    require => Exec['download composer'],
-    group => 'staff',
-    mode => '0755'
-  }
-
-# update composer
-  exec { "update composer":
-    command => "/usr/local/bin/composer selfupdate",
-    require => File["/usr/local/bin/composer"]
+  file { '/usr/local/bin/composer':
+    mode => '0555',
+    owner => root,
+    group => root,
+    require => Exec['download composer']
   }
 }
