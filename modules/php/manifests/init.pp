16cf95bf2b9c83f87ab83f0abaafa1de61deadff
Sam-Mauris Yong <mauris@hotmail.sg>
2013-07-21 14:33:46 +0800

    added libapache2-mod-php5

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -1,5 +1,5 @@
 class php {
-  $packages = ["php5", "php5-cli", "php5-mysql", "php-pear", "php5-dev", "php5-gd"]
+  $packages = ["php5", "php5-cli", "php5-mysql", "php-pear", "php5-dev", "php5-gd", "libapache2-mod-php5"]
   
   package { $packages:
     ensure => present,
