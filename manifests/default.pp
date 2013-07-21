7a4fb2421e4e7ca7c37f45c0f0dbae423ddc241a
Sam-Mauris Yong <mauris@hotmail.sg>
2013-07-21 12:59:13 +0800

    mod rewrite installation

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,7 @@
 include system
 include system::bootstrap
 include apache
+include apache::mod::rewrite
 include php
 include php::pear
 include php::composer
---
+++ modules/apache/manifests/mod/rewrite.pp
@@ -0,0 +1,10 @@
+class apache::mod::rewrite {
+    include apache
+# ensures that mode_rewrite is loaded and modifies the default configuration file
+  file { "/etc/apache2/mods-enabled/rewrite.load":
+    ensure => link,
+    target => "/etc/apache2/mods-available/rewrite.load",
+    require => Package["apache2"]
+  }
+  
+}
