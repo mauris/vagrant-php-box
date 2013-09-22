e8bd1788491846d46ba05ada6e59933daf9cfb32
Sam-Mauris Yong <mauris@hotmail.sg>
2013-09-22 22:41:33 +0800

    system tools installation

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -2,4 +2,10 @@
 exec{ 'apt-update':
     command => "/usr/bin/apt-get update"
 }
+
+$packages = ["curl", "vim", "git"]
+package { $packages:
+    ensure => present,
+    require => Exec["apt-update"]
+}
 include composer
