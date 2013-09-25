04f0c8cee387db6ba496ede30ac581beebb89247
Sam-Mauris Yong <mauris@hotmail.sg>
2013-09-25 22:48:58 +0800

    updated pear upgrrade and packages

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,14 +1,21 @@
 class php {
   package {'php5':
-    ensure => present,
+    ensure => installed,
     alias => 'php',
-    require => Exec['apt-update']
+    require => Exec['apt-update'],
+    notify => Service['apache2']
   }
 
-  $packages = ["php5-cli", "php5-mysql", "php-pear", "php5-dev", "php5-gd", "libapache2-mod-php5"]
+  $packages = ["php5-cli", "php5-cgi", "php5-mysql", "php-pear", "php5-dev", "php5-gd"]
 
   package { $packages:
-    ensure => present,
-    require => Exec["apt-update"]
+    ensure => installed,
+    require => Exec["apt-update"],
+    notify => Service['apache2']
+  }
+
+  exec { "pear upgrade":
+    command => "/usr/bin/pear upgrade",
+    require => Package["php-pear"]
   }
 }
