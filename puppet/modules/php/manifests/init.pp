d8e2985e1c6fa4f2f9ca45d41087cc558368c142
Sam-Mauris Yong <mauris@hotmail.sg>
2013-10-13 11:57:30 +0800

    pear phpunit and phpcs installation

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -18,4 +18,19 @@ class php {
     command => "/usr/bin/pear upgrade",
     require => Package["php-pear"]
   }
+
+  exec { "pear auto_discover":
+    command => "/usr/bin/pear config-set auto_discover 1",
+    require => Package["php-pear"]
+  }
+
+  exec { "phpunit-install":
+    command => "/usr/bin/pear install pear.phpunit.de/PHPUnit",
+    require => Package["php-pear"]
+  }
+
+  exec { "phpcs-install":
+    command => "/usr/bin/pear install PHP_CodeSniffer",
+    require => Package["php-pear"]
+  }
 }
